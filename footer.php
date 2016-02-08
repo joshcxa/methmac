bdda4078f184cf0f18e8fac60a4c0b927767a2a9
Josh <joshcxa@gmail.com>
2016-02-08 21:22:23 +1100

    Remove scripts
    Now enqueing scripts from functions.php

--- footer.php
+++ footer.php
@@ -22,10 +22,6 @@
     </div>
 </footer>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-<script src="<?php bloginfo( 'template_directory' ); ?>/js/bootstrap.min.js"></script>
-<script src="<?php bloginfo( 'template_directory' ); ?>/js/scripts.js"></script>
-
 <?php wp_footer(); ?> 
 
 </body>
